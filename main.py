afed60e6277f81c1da534d5f5f4bd128c6548bbf
MarkusH17 <markushughes1733@gmail.com>
2022-06-07 12:37:38 -0700

    updated main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #import directories
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 num = np.exp(3)
 print(num)
+xs = np.linspace(0,100,101)
+ys = xs**2
+plt.plot(xs,ys)
+plt.show()

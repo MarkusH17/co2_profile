14e61b27d285e2c94a380a5a8922647a5f19eda4
MarkusH17 <markushughes1733@gmail.com>
2022-06-07 14:53:43 -0700

    -a

---
+++ consumption.py
@@ -0,0 +1,10 @@
+#import directories
+import numpy as np
+import matplotlib.pyplot as plt
+import diffusionprofile as dp
+import pandas as pd
+
+#parameters
+area = 7500 #m^2
+height = 6.5 #m
+plants = 1.5 #m

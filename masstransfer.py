eb20dbcfa6b3ed8be6f709cc69632378883283cd
MarkusH17 <markushughes1733@gmail.com>
2022-06-07 19:34:32 -0700

    push to sync with new computer

--- masstransfer.py
+++ masstransfer.py
@@ -1,1 +1,10 @@
 import numpy as np
+
+#properties of air, using linear interpolation
+#from Bergman and Lavine, Fundamentals of Heat and Mass Transfer 8e
+def density(T):
+    
+    return rho
+mu = 184.6/1e7 #N*s/m2
+#hydraulic diameter assumption
+reynolds = rho*vel*Dh/mu
